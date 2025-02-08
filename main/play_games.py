ef67367b2a487d9c25dcd85d4e5b06ca15b4f0ab
Abey <abrahamborg@gmail.com>
2025-02-08 11:39:20 -0800

    added code to print all currupt games

--- main/play_games.py
+++ main/play_games.py
@@ -24,11 +24,13 @@ if not logger.handlers:
 
 if __name__ == '__main__':
     start_time = time.time()
+    num_games_to_play = 10 + 1 # add one more for the range function
+
     # !!!!!!!!!!!!!!!!! change this each time for new section of the database  !!!!!!!!!!!!!!!!!
     # chess_data = pd.read_pickle(game_settings.chess_games_filepath_part_1, compression='zip')
     # chess_data = chess_data.head(10000)
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    for part in range(1, 101):
+    for part in range(1, num_games_to_play):
         # Dynamically retrieve the file path from game_settings.
         file_path = getattr(game_settings, f'chess_games_filepath_part_{part}')
         
@@ -36,6 +38,7 @@ if __name__ == '__main__':
             chess_data = pd.read_pickle(file_path, compression='zip')
             corrupted_games = play_games(chess_data)           
             chess_data = chess_data.drop(corrupted_games)
+            print(f"Part {part}: {len(corrupted_games)} corrupted games detected.")
             chess_data.to_pickle(file_path, compression='zip')        
         except Exception as e:
             print(f"Error processing {file_path}: {e}")
@@ -45,5 +48,4 @@ if __name__ == '__main__':
         
     end_time = time.time()
     total_time = end_time - start_time
-    print(f'corrupt games list length: {len(corrupted_games)}')
     print(f'total time: {total_time} seconds')
